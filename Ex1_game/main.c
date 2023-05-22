4e691b167f5a166a21d81568a80d977ce15ba00d
MisterCHOUN <65115291+MisterCHOUN@users.noreply.github.com>
2023-05-22 16:24:01 +0200

    fix a bit the c file + adding python file for test

--- Ex1_game/main.c
+++ Ex1_game/main.c
@@ -1,117 +1,139 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(){
     printf("[Initialisation]\n");
     printf("[POD 099 at your service, I am a robot existing to serve you]\n");
-    printf("[Please let us create your Character :]\n");
+    printf("[Please let us write your description :]\n");
 
+    char human_confirmation[3];
+    char gender[10];
+    int age;
+    char day[2], month[2], year[4];
+    char hair_color[15];
+    char eye_color[15];
+    int height;
+    int weight;
+    char job[20];
+    char like[20];
+    char dislike[20];
+    char strong_point[20];
+    char weak_point[20];
+    char house[20];
+    int n_lan;
+    char born[20];
+    char relation[20];
+    char accomplishment[20];
+    char fear[20];
+    char future[20];
     //1
     printf("[Are you a human ?]\n");
-    char human_confirmation;
     printf("Yes / no -> ");
-    scanf("%c", &human_confirmation);
+    scanf("%s", &human_confirmation);
+    printf("tes = %s", human_confirmation);
     //2
     printf("\n[What is your Gender ?]\n");
-    char gender;
     printf("Male, Female, Non-binary -> ");
-    scanf("%c", &gender);
+    scanf("%s", &gender);
     //3
     printf("\n[How old are you ?]\n");
-    int age;
-    printf("Enter a Number :");
-    scanf("%d", &gender);
+    printf("Enter a Number : ");
+    scanf("%d", &age);
     //4
     printf("\n[What is your birthdate ?]\n");
-    char day; month; year;
-    printf("Please enter : Day number, month number, year number like 01 02 2000 -> ");
-    scanf("%c", &day);
-    scanf("%c", &month);
-    scanf("%c", &year);
+    printf("Please enter : Day number, month number, year number like 01 02 2000 \n day -> ");
+    scanf("%s", &day);
+    printf("\nmonth -> ");
+    scanf("%s", &month);
+    printf("\nyear -> ");
+    scanf("%s", &year);
     //5
     printf("\n[What is your hair color ?]\n");
-    char hair_color;
     printf("-> ");
-    scanf("%c", &hair_color);
+    scanf("%s", &hair_color);
     //6
     printf("\n[What colour are your eyes ?]\n");
-    char eye_color;
     printf("-> ");
-    scanf("%c", &eye_color);
+    scanf("%s", &eye_color);
     //7
     printf("\n[What is your height ?]\n");
-    int height;
     printf("Enter a Number in cm : ");
     scanf("%d", &height);
     //8
     printf("\n[What is your weight ?]\n");
-    int weight;
     printf("Enter a Number in kg : ");
     scanf("%d", &height);
     //9
     printf("\n[What is your profession ?]\n");
-    char job;
     printf("-> ");
-    scanf("%c", &job);
+    scanf("%s", &job);
     //10
     printf("\n[What do you like ?]\n");
-    char like;
     printf("-> ");
-    scanf("%c", &like);
+    scanf("%s", &like);
     //11
     printf("\n[What do you hate ?]\n");
-    char dislike;
     printf("-> ");
-    scanf("%c", &dislike);
+    scanf("%s", &dislike);
     //12
     printf("\n[Gime me one of your strong point ?]\n");
-    char strong_point;
     printf("-> ");
-    scanf("%c", &strong_point);
+    scanf("%s", &strong_point);
     //13
     printf("\n[Gime me one of your weak point ?]\n");
-    char weak_point;
     printf("-> ");
-    scanf("%c", &weak_point);
+    scanf("%s", &weak_point);
     //14
     printf("\n[Where do you live ?]\n");
-    char house;
     printf("-> ");
-    scanf("%c", &house);
+    scanf("%s", &house);
     //15
     printf("\n[How many Languages can you speak ?]\n");
-    int n_lan;
     printf("-> ");
     scanf("%d", &n_lan);
     //16
     printf("\n[Where are you born ?]\n");
-    char born;
     printf("-> ");
-    scanf("%c", &born);
+    scanf("%s", &born);
     //17
     printf("\n[Are you in a relationship ?]\n");
-    char relation;
     printf("yes / no -> ");
-    scanf("%c", &relation);
+    scanf("%s", &relation);
     //18
     printf("\n[What is your greatest accomplishment ?]\n");
-    char accomplishment;
     printf("-> ");
-    scanf("%c", &accomplishment);
+    scanf("%s", &accomplishment);
     //19
     printf("\n[Do you fear death ?]\n");
-    char fear;
     printf("yes / no -> ");
-    scanf("%c", &fear);
+    scanf("%s", &fear);
     //20
     printf("\n[Any plan for your future ?]\n");
-    char future;
     printf("-> ");
-    scanf("%c", &future);
+    scanf("%s", &future);
 
 
-    printf("\n\n[Here are your resume :]\n")
-    printf("\n")
-
+    printf("\n\n[Here are your resume :]\n");
+    printf("human_confirmation = %s\n",human_confirmation);
+    printf("gender = %s\n",gender);
+    printf("human_confirmation = %d\n",age);
+    printf("Birthdate = %s/%s/%s\n",day, month, year);
+    printf("Hair Color = %s\n",hair_color);
+    printf("Eye Color = %s\n",eye_color);
+    printf("Height = %d cm\n",height);
+    printf("Weight = %d kg\n",weight);
+    printf("Job = %s\n",job);
+    printf("Like = %s\n",like);
+    printf("Dislike = %s\n",dislike);
+    printf("Strong Point = %s\n",strong_point);
+    printf("Weak Point = %s\n",weak_point);
+    printf("Living in = %s\n",house);
+    printf("Number of Languages learned = %d\n",n_lan);
+    printf("Place of Birth = %s\n",born);
+    printf("Relationship = %s\n",relation);
+    printf("Biggest Accomplisment = %s\n",accomplishment);
+    printf("Fear of Death = %s\n",fear);
+    printf("Future Plans = %s\n",future);
 
     return 0;
 }
